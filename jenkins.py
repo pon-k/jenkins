450003bb00dc134af35a75222673cf93793da68a
pon-k <78046862+pon-k@users.noreply.github.com>
2021-10-31 12:26:41 +0000

    function update

--- jenkins.py
+++ jenkins.py
@@ -37,50 +37,74 @@ async def on_ready():
 async def greeting(ctx):
     await ctx.send('Greetings.')
 
-@bot.command(name='dadjoke')
+@bot.command(name='dadjoke', help='Presents a random dad joke')
 async def dad_joke(ctx):
     await ctx.send(random.choice(jokes))
 
-@bot.command(name='wisdom')
+@bot.command(name='wisdom', help='Presents a random wisdom')
 async def sayings(ctx):
     await ctx.send(random.choice(quotes))
 
-@bot.command(name='coinflip', help='Flip a coin.')
+@bot.command(name='coinflip', help='Flips a coin')
 async def c_flip(ctx):
     c_result = ['Heads', 'Tails']
     await ctx.send(f':coin: The result is {random.choice(c_result)}. :coin: ')
 
-@bot.command(name='roll', help='Roll a die.')
+@bot.command(name='roll', help='Rolls a die. Will default to six-sided if a number is not given')
 async def dice(ctx, sides=6):
     d_roll = random.randint(1, sides)
     await ctx.send(f':game_die: You rolled a {d_roll}. :game_die:')
 
-@bot.command(name='smug')
+@bot.command(name='smug', help='Presents smug')
 async def anismug(ctx):
     path = random.choice(os.listdir('smug/'))
     await ctx.send(file=discord.File('smug/' + path))
+
+@bot.command(name='jankenpon', help='Play rock paper scissors with the bot')
+async def rps(ctx, choice):
+    choice = choice.capitalize()
+    comp = random.choice(['Rock', 'Paper', 'Scissors'])
+    comparison = {'Rock': 'Scissors', 'Scissors': 'Paper', 'Paper': 'Rock'}
+    if comparison[choice] == comp:
+        await ctx.send(f'{comp}. You win!')
+    elif comp == choice:
+        await ctx.send(f'{comp}. It\'s a draw.')
+    else:
+        await ctx.send(f'{comp}. You lose.')
+
 
 @bot.command(name='admincheck')
 @has_permissions(administrator=True)
 async def adcheck(ctx):
     await ctx.send('Admin check passed')
 
-@bot.command(name='nickname')
+@bot.command(name='nickname', help='Changes a user\'s nickname. ADMIN')
 @has_permissions(administrator=True)
 async def change_nickname(ctx, member: discord.Member, nname):
     await member.edit(nick=nname)
     await ctx.send(f'Nickname has been changed to {nname}.')
 
-@bot.command(name='createtc')
+@bot.command(name='createtc', help='Creates a text channel. Requires a name for the new channel. ADMIN')
 @has_permissions(administrator=True)
 async def create_txt(ctx, cname):
     await ctx.message.guild.create_text_channel(cname)
     await ctx.send(f'Text channel **{cname}** created.')
 
-@bot.command(name='createvc')
+@bot.command(name='createvc', help='Creates a voice channel. Requires a name for the new channel. ADMIN')
 @has_permissions(administrator=True)
 async def create_vc(ctx, cname):
     await ctx.message.guild.create_voice_channel(cname)
     await ctx.send(f'Voice channel **{cname}** created.')
+
+@bot.command(name='addrole', help='Adds a new role to the server. ADMIN')
+@has_permissions(administrator=True)
+async def add_role(ctx, rname=None):
+    if not rname:
+        await ctx.send('You must choose a name for the new role.')
+    else:
+        await ctx.guild.create_role(name=rname)
+        await ctx.send(f'New role **{rname}** created.')
+
+
 
 bot.run(TOKEN)
